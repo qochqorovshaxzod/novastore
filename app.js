3953d547e86ed635912f278485a385e57c183555
shaxzod09 <shaxzodqochqorov477@gmail.com>
2026-08-05 13:37:52 +0500

    Add welcome alert and button click functionality

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("NovaStore ishga tushdi!");
+alert("NovaStore saytiga xush kelibsiz!");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button=>{
+button.addEventListener("click",()=>{
+alert("Yuklab olish tez orada ishga tushadi!");
+});
+});
